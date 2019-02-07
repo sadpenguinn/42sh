7fd0b71aea42c27bc28b5e3e435f10eec070596b
Roman Tsygankov (sitlcead) <ryatsygankov@gmail.com>
2019-02-07 10:04:46 +0300

    some features for redline/

--- srcs/readline/array.c
+++ srcs/readline/array.c
@@ -1,14 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   readline.c                                         :+:      :+:    :+:   */
+/*   array.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: narchiba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/06 20:04:58 by narchiba          #+#    #+#             */
-/*   Updated: 2019/02/07 08:48:26 by narchiba         ###   ########.fr       */
+/*   Created: 2019/02/07 08:40:42 by narchiba          #+#    #+#             */
+/*   Updated: 2019/02/07 09:14:41 by narchiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include "shell.h"
+#include "array.h"
+#include <string.h>
+
+static t_array	*arr;
+
+void	array_add(const char *str, int len)
+{
+	if (arr == NULL)
+	{
+		arr = (t_array *)xmalloc(sizeof(t_array));
+		arr->buf = (char *)xmalloc
+	}
+}
--- srcs/readline/array.h
+++ srcs/readline/array.h
@@ -1,14 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   readline.c                                         :+:      :+:    :+:   */
+/*   array.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: narchiba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/06 20:04:58 by narchiba          #+#    #+#             */
-/*   Updated: 2019/02/07 08:48:26 by narchiba         ###   ########.fr       */
+/*   Created: 2019/02/07 08:44:45 by narchiba          #+#    #+#             */
+/*   Updated: 2019/02/07 09:08:23 by narchiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include "shell.h"
+#ifndef ARRAY_H
+# define ARRAY_H
+
+# define ARRAY_SIZE 10
+# define FACTOR 2
+
+typedef	struct	s_array
+{
+	char	*buf;
+	int		len;
+}				t_array;
+
+void	array_add(const char *str, int len);
+
+#endif
