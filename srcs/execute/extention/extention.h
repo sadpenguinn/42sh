2fe9d45915894fa4ecd315758c5afaf3a7c8871f
bbaelor- / Valeriy Klimenko <shnurd6@mail.ru>
2019-02-19 19:46:43 +0300

    update extention, work

--- srcs/execute/extention/extention.h
+++ srcs/execute/extention/extention.h
@@ -6,23 +6,25 @@
 /*   By: bbaelor- <bbaelor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/16 16:50:55 by bbaelor-          #+#    #+#             */
-/*   Updated: 2019/02/16 17:51:59 by bbaelor-         ###   ########.fr       */
+/*   Updated: 2019/02/19 19:10:46 by bbaelor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXTENTION_H
 # define EXTENTION_H
 
+#include "libshell.h"
+
 /*
 ** ${parameter:-word} 1
 ** ${parameter:=word} 2
 ** ${parameter:?word} 3
 ** ${parameter:+word} 4
-** ${#parameter}      5  
-** ${parameter%}      6  
-** ${parameter%%}     7  
-** ${parameter#}      8  
-** ${parameter##}     9  
+** ${#parameter}      5
+** ${parameter%}      6
+** ${parameter%%}     7
+** ${parameter#}      8
+** ${parameter##}     9
 */
 
 char	*get_content_of_expression(char *str);
