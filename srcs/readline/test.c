a531f1006898c1450e22cb20ea5f9b1c7d7b745c
Roman Tsygankov (sitlcead) <ryatsygankov@gmail.com>
2019-02-24 12:55:03 +0300

    readline prompt bash style

--- srcs/readline/test.c
+++ srcs/readline/test.c
@@ -6,7 +6,7 @@
 /*   By: sitlcead <sitlcead@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/20 14:13:07 by sitlcead          #+#    #+#             */
-/*   Updated: 2019/02/20 19:32:27 by sitlcead         ###   ########.fr       */
+/*   Updated: 2019/02/24 08:40:42 by sitlcead         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,23 @@
 #include <unistd.h>
 #include "term.h"
 #include <fcntl.h>
+#include <string.h>
+
+void     ft_puts(char *buf, int len)
+{
+	if (len == 0)
+		len = strlen(buf);
+	write(1, buf, len);
+}
 
 int main(void)
 {
+/*	int i = 0;
+	while (++i < 100)
+		ft_puts("a", 0);*/
+	ft_puts("\e[3A\e3D", 0);
+/*	char c;
+	read(0, &c, 1);
 	unsigned char c = 0;
 	set_term();
 	read(0, &c, 1);
@@ -25,6 +39,6 @@ int main(void)
 		printf("%d\n", c);
 		read(0, &c, 1);
 	}
-	unset_term();
+	unset_term();*/
 	return (0);
 }
