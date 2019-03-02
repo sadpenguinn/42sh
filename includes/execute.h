4905cb93fa5fd323246cafa9abe45b603364c0f6
Triggerr <disident47@gmail.com>
2019-03-02 20:56:08 +0300

    Changed includes/execute.h to hardlink from srcs/execute/execute.h

--- includes/execute.h
+++ includes/execute.h
@@ -6,119 +6,116 @@
 /*   By: bwerewol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 16:27:11 by bwerewol          #+#    #+#             */
-/*   Updated: 2019/02/26 17:32:22 by nkertzma         ###   ########.fr       */
+/*   Updated: 2019/02/27 22:45:42 by nkertzma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "shell.h"
-#include "libft.h"
-/* #include "parser.h" */
-#include <stdio.h>
-#include "pthread.h"
-extern int	g_execerr;
+#ifndef EXECUTE_H
+# define EXECUTE_H
 
-/* #define EXECUTE_DEBUG */
+# include "shell.h"
+# include "libft.h"
+# include <stdio.h>
+
+# define EXECUTE_RELEASE
+
+extern int				g_execerr;
+extern void				*g_pids;
+
 /*
-** redir types and struct
+**	Flags for g_redirf
+*/
+
+# define REDIR_NO		0
+# define REDIR_IN		0b1
+# define REDIR_OUT		0b01
+# define REDIR_IO		0b11
+
+/*
+**	Flags for t_redir->type
 */
 
 # define REDIRECT		1
 # define CLOSEFD		2
 
-typedef struct	s_redir
+/*
+**	Flags for job
+*/
+
+# define EX_NOFG		0
+# define EX_FG			1
+
+typedef struct			s_redir
 {
 	int		type;
 	int		fd[2];
-}				t_redir;
+}						t_redir;
 
-typedef struct	s_cmd
+typedef struct			s_cmd
 {
 	t_astree	*root;
-	void 		*ppid;
+	void		*ppid;
 	int			fd[2];
 	int			job;
-}				t_cmd;
+}						t_cmd;
 
-int		execute(t_astree *root);
-int		execlist1(t_astree *root, int fd[2], int job, int isfork);
-int		execlist2(t_astree *root, int fd[2], int job, int isfork);
-int		execlist3(t_astree *root, int fd[2], int job, int isfork);
-int		execpipecmd(t_astree *root, int fd[2], int job, int isfork);
-int		execpipes(t_astree	*root, int fd[2], int job, int isfork);
-int		execcmd(t_astree *root, int fd[2], int job, int isfork);
-int		execscmd(t_astree *root, int fd[2], int job, int isfork);
+typedef struct			s_func
+{
+	char		*name;
+	t_astree	*func;
+}						t_func;
 
-int		execcase(t_astree *root, int fd[2], int job, int isfork);
-int		execif(t_astree *root, int fd[2], int job, int isfork);
-int		execsubshell(t_astree *root, int fd[2], int job, int isfork);
-int		function(t_astree *func, char **argv, int fd[2], int job, int isfork);
+int						execute(t_astree *root);
+int						execlist1(t_astree *root,
+									int fd[2], int job, int isfork);
+int						execlist2(t_astree *root,
+									int fd[2], int job, int isfork);
+int						execlist3(t_astree *root,
+									int fd[2], int job, int isfork);
+int						execpipecmd(t_astree *root,
+									int fd[2], int job, int isfork);
+int						execpipes(t_astree	*root,
+									int fd[2], int job, int isfork);
+int						execcmd(t_astree *root,
+									int fd[2], int job, int isfork);
+int						execshellcmd(t_astree *root,
+									int fd[2], int job, int isfork);
+int						execscmd(t_astree *root,
+									int fd[2], int job, int isfork);
 
-# define EX_NOFG	0
-# define EX_FG		1
+int						execfor(t_astree *root,
+									int fd[2], int job, int isfork);
+int						execcase(t_astree *root,
+									int fd[2], int job, int isfork);
+int						execif(t_astree *root,
+									int fd[2], int job, int isfork);
+int						execsubshell(t_astree *root,
+									int fd[2], int job, int isfork);
+int						execfunction(t_astree *root);
+int						execwhile(t_astree *root,
+									int fd[2], int job, int isfork);
+int						execuntil(t_astree *root,
+									int fd[2], int job, int isfork);
 
-t_redir	*get_redir(t_astree *root);
+t_redir					*get_redir(t_astree *root);
+int						applyredir(t_list *redirs);
 
-int pipeerror(void);
-int fileerror(char *file);
-int forkerror(char *cmd);
-int threaderror(void);
+pid_t					xfork(void);
+pid_t					xwaitpid(pid_t pid, int *stat_loc, int options);
+
+int						pipeerror(void);
+int						fileerror(char *file);
+int						forkerror(char *cmd);
+int						threaderror(void);
+int						cmdexitsig(pid_t pid, int sig);
+t_redir					*redirfileerror(char *file, t_redir *redir);
+
 /*
-**	<|Parser defines|>
-**
-** (	OBRACKET
-** )	CBRACKET
-** {	OBRACE
-** }	CBRACE
-** [	OARRAY
-** ]	CARRAY
-** &	AND
-** ;	SEMI
-** &&	AND_IF
-** ||	OR_IF
-** ;;	DSEMI
-** ;&	SEMI_AND
-** ;;&	DSEMI_AND
-** <	LESS
-** >	GREAT
-** <&	LESSAND
-** >&	GREATAND
-** <<	DLESS
-** >>	DGREAT
-** <>	LESSGREAT
-** >|	CLOBBER
-** <<-	DLESSDASH
-** <<<	TLESS
-** V=W	ASSIGMENT_WORD
-** 123	NUMBER
-** \n	NEWLINE
-** !	NOT
-** |	PIPE
-** |&	PIPEAND
-** IF
-** THEN
-** ELSE
-** ELIF
-** FI
-** FOR
-** WHILE
-** UNTIL
-** DO
-** DONE
-** CASE
-** IN
-** ESAC
-** FUNCTION
-** SELECT
-** COPROC
-**
-** COMMAND
-** REDIRECTION
-** TOKEOF
-** ARITH
-** COND			[[ ... ]]
-** REST
-** CMDREDIR
-** PIPECMD
-**
+**	initcmd.c
+**	Init command args
 */
 
+int						initcmd(t_astree *root, int fd[2], t_list *cmd[3], char **aven[2]);
+int						add_pipe_redir(t_list **redlst, int fd[2]);
+
+#endif
