d5cb3de24257b99fbf023506b26df9a5b2658ee0
Triggerr <disident47@gmail.com>
2019-03-03 17:22:44 +0300

    Fix function adding

--- srcs/execute/execfunction.c
+++ srcs/execute/execfunction.c
@@ -11,11 +11,10 @@
 
 int		execfunction(t_astree *root)
 {
-	t_func	*func;
+	t_func	func;
 
-	func = xmalloc(sizeof(t_func));
-	func->name = ft_strdup(root->content);
-	func->func = root->right;
+	func.name = ft_strdup(root->content);
+	func.func = root->right;
 	root->right = 0;
 	vector_push_back(&g_func, &func);
 	printf("function created\n");
