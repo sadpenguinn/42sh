ecd286181944855c012753d3200a9256075fc010
Triggerr <disident47@gmail.com>
2019-03-03 17:41:19 +0300

    Fix functions update

--- srcs/execute/execfunction.c
+++ srcs/execute/execfunction.c
@@ -8,15 +8,32 @@
 */
 
 #include "execute.h"
+#include "parser.h"
 
 int		execfunction(t_astree *root)
 {
+	t_func	*function;
+	size_t	size;
+	size_t	i;
 	t_func	func;
 
+	i = 0;
+	size = vector_get_len(g_func);
+	while (i < size)
+	{
+		function = (t_func *)vector_get_elem(g_func, i);
+		if (!ft_strcmp(function->name, root->content))
+		{
+			freeastree(function->func);
+			function->func = root->right;
+			root->right = 0;
+			return (EXIT_SUCCESS);
+		}
+		i++;
+	}
 	func.name = ft_strdup(root->content);
 	func.func = root->right;
 	root->right = 0;
 	vector_push_back(&g_func, &func);
-	printf("function created\n");
 	return (EXIT_SUCCESS);
 }
