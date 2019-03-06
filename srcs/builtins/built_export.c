90db619830bf2e60e069cad3b7b3f94de346643a
Vladislav Badrudin (sadpenguinn) <alexerrr@pm.me>
2019-03-06 14:32:48 +0300

    Fix '' segf, added readline lexer

---
+++ srcs/builtins/built_export.c
@@ -0,0 +1,37 @@
+#include "builtins.h"
+
+/*
+static int		parse_flags(char **av, char **env)
+{
+	if (!ft_strcmp(av[1], "-n"))
+	{
+		nil
+	}
+	if (!ft_strcmp(av[1], "-p"))
+	{
+		if (!av[2])
+	}
+	return (SHERR_OK);
+
+}
+
+int 			built_export(char **av, char **env)
+{
+	if (av[1])
+		parse_flags(av, env);
+	else
+		print_str_arr(env);
+	return (SHERR_OK);
+}
+
+int				built_export(char **av, char **env)
+{
+	char	*key;
+	char	*value;
+
+	env = NULL;
+	if (!parse_flags(av, &key, &value))
+		return (SHERR_ERR);
+	ssetenv(key, value);
+	return (SHERR_OK);
+}*/
--- srcs/lib/libft/srcs/ft_strchr.c
+++ srcs/lib/libft/srcs/ft_strchr.c
@@ -14,6 +14,8 @@
 
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	while (*s)
 	{
 		if (*s == (char)c)
