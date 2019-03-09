7dfc2403158a9574ac628f02025382653aa32c69
Vladislav Badrudin (sadpenguinn) <alexerrr@pm.me>
2019-03-09 18:05:50 +0300

    Refactored

--- srcs/builtins/built_set_rqmts.c
+++ srcs/builtins/built_set_rqmts.c
@@ -1,4 +1,48 @@
-//
-// Created by Niruin Kertzmann on 2019-03-09.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   built_set_reqmts.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkertzma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/08 13:31:41 by nkertzma          #+#    #+#             */
+/*   Updated: 2019/03/08 13:31:42 by nkertzma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "shell.h"
+#include "readline.h"
+#include "builtins.h"
+
+int		built_set_usage(void)
+{
+	ft_putendl("Huita");
+	return (SHERR_ERR);
+}
+
+int		built_set_toggle(int *var, int fval, int sval)
+{
+	if (*var == fval)
+		*var = sval;
+	else
+		*var = fval;
+	return (SHERR_OK);
+}
+
+int		built_set_gvar(int *var, int val)
+{
+	*var = val;
+	return (SHERR_OK);
+}
+
+void	built_set_print(void)
+{
+	ft_putstr("vi:\t\t");
+	ft_putendl(g_mode == VI ? "on" : "off");
+	ft_putstr("emacs:\t\t");
+	ft_putendl(g_mode == READLINE ? "on" : "off");
+	ft_putstr("echoe:\t\t");
+	ft_putendl(g_echoe == TRUE ? "on" : "off");
+	ft_putstr("syntax:\t\t");
+	ft_putendl(g_syntax == SYNTAX_ON ? "on" : "off");
+}
