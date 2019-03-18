21420abf6041aa548ff2cef83c1fd6fc79dd6eb0
Roman Tsygankov (sitlcead) <ryatsygankov@gmail.com>
2019-03-18 09:55:57 +0300

    fc - add some options

--- srcs/builtins/built_fc.c
+++ srcs/builtins/built_fc.c
@@ -3,27 +3,24 @@
 #include "get_next_line.h"
 #include "readline.h"
 #include <pwd.h>
-
-static void	read_history_from_file(int fd, void **vector)
-{
-	int			ret;
-	char		*str;
-
-	while ((ret = get_next_line(fd, &str)))
-		vector_push_back(vector, &str);
-}
+#include "lexer.h"
+#include "parser.h"
+#include "execute.h"
 
 static void	read_history_from_disk(void **vector)
 {
 	int				fd;
 	char			*file_name;
 	struct passwd	*pw;
+	int				ret;
+	char			*str;
 
 	pw = getpwuid(getuid());
 	file_name = ft_strjoin(pw->pw_dir, "/", 0);
 	file_name = ft_strjoin(file_name, HISTORY_FILE, 1);
 	fd = open(file_name, O_CREAT | O_RDONLY | O_APPEND, 0777);
-	read_history_from_file(fd, vector);
+	while ((ret = get_next_line(fd, &str)))
+		vector_push_back(vector, &str);
 	close(fd);
 	free(file_name);
 }
@@ -44,7 +41,28 @@ static void	fc_history_free(void *fc_history)
 	}
 }
 
-int					built_fc(char **av, char **env)
+static int		parse_string(char *script)
+{
+	t_lexer		*lex;
+	t_astree	*ast;
+	int 		last;
+	char 		*tmp;
+
+	ft_putstr(script);
+	lex = lexer(script, ft_strlen(script));
+	ft_strdel(&script);
+	g_tokens = lex->lexems;
+	ast = inputunit();
+	last = execute(ast);
+	tmp = ft_itoa(last);
+	ssetenv("?", tmp, ENV_RO);
+	ft_strdel(&tmp);
+	freeastree(ast);
+	lexer_free(lex);
+	return (1);
+}
+
+int			built_fc(char **av, char **env)
 {
 	int		flags[4];
 	void	*fc_history;
@@ -54,7 +72,7 @@ int					built_fc(char **av, char **env)
 	fc_history = vector_create(sizeof(void *));
 	read_history_from_disk(&fc_history);
 	str = (char **)vector_front(fc_history);
-	printf("%s\n", *str);
+	parse_string(*str);
 	fc_history_free(fc_history);
 	vector_free(&fc_history);
 	(void)flags;
