911340d83effbf6bd24eab2c4f1053e003cfd891
bbaelor- / Valeriy Klimenko <shnurd6@mail.ru>
2019-03-26 02:59:23 +0300

    Norminated conditions part

--- srcs/conditions/main_and_debug.c
+++ srcs/conditions/main_and_debug.c
@@ -6,7 +6,7 @@
 /*   By: bbaelor- <bbaelor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 17:50:20 by bbaelor-          #+#    #+#             */
-/*   Updated: 2019/03/26 02:18:21 by bbaelor-         ###   ########.fr       */
+/*   Updated: 2019/03/26 02:58:52 by bbaelor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,6 @@
 
 void			*g_tokens = NULL;
 unsigned int	g_curtok = 0;
-
-// void	printmas(char **str)
-// {
-// 	int i;
-
-// 	i = 0;
-// 	while (str[i])
-// 	{
-// 		free(str[i]);
-// 		i++;
-// 	}
-// 	free(str);
-// }
-
-// int		main(void)
-// {
-// 	printf("%d\n", conditions(strsplit_to_conditions("")));
-// 	return (0);
-// }
 
 int		main(int argc, char **argv, char **env)
 {
--- srcs/conditions/strsplit_conditions.c
+++ srcs/conditions/strsplit_conditions.c
@@ -6,66 +6,11 @@
 /*   By: bbaelor- <bbaelor-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/20 20:45:31 by bbaelor-          #+#    #+#             */
-/*   Updated: 2019/03/26 02:24:25 by bbaelor-         ###   ########.fr       */
+/*   Updated: 2019/03/26 02:57:38 by bbaelor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "conditions.h"
-
-// static char		*validation_brackets(char *str)
-// {
-// 	int		len;
-// 	char	*res;
-
-// 	if (!str)
-// 		return (NULL);
-// 	len = ft_strlen(str);
-// 	if (!(str[0] == '[' && str[1] == ' ' &&
-// 		str[len - 1] == ']') && str[len - 2] == ' ')
-// 		return (NULL);
-// 	if (len - 4 < 1)
-// 		return (0);
-// 	res = ft_strndup(&str[2], len - 4);
-// 	printf("res = |%s|\n", res);
-// 	free(str);
-// 	return (res);
-// }
-
-// static int		ft_word_count(char *s)
-// {
-// 	int count;
-
-// 	count = 0;
-// 	while (*s)
-// 	{
-// 		while (ft_isspace(*s) && *s)
-// 			s++;
-// 		if (*s)
-// 			count++;
-// 		while (!ft_isspace(*s) && *s)
-// 			s++;
-// 	}
-// 	return (count);
-// }
-
-// static char		*ft_word_cpy(char **s)
-// {
-// 	int		len;
-// 	char	*str;
-// 	char	*rtn;
-
-// 	while (ft_isspace(**s))
-// 		(*s)++;
-// 	len = 0;
-// 	while ((*s)[len] && ft_isspace((*s)[len]))
-// 		len++;
-// 	str = (char *)xmalloc(sizeof(char) * (len + 1));
-// 	rtn = str;
-// 	while (**s && !ft_isspace(**s))
-// 		*str++ = *(*s)++;
-// 	*str = 0;
-// 	return (rtn);
-// }
 
 static void		free_mas(char **str)
 {
@@ -92,7 +37,6 @@ static char		**rewriting_mas(char **str, int len)
 	while (i < len)
 	{
 		res[i - 1] = expand(str[i]);
-		// printf("Записал в res[%d] = |%s|\n", i - 1, res[i - 1]);
 		free(str[i]);
 		i++;
 	}
@@ -108,13 +52,9 @@ char			**strsplit_to_conditions(char *str)
 	int		i;
 
 	i = 0;
-	// printf("get string %s\n", str);
 	res = ft_strsplit(str, ' ');
 	while (res[i])
-	{
-		// printf("res[%d] = |%s|\n", i, res[i]);
 		i++;
-	}
 	if (!res)
 	{
 		free(str);
